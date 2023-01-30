8c3ce165dc1ff30f160bdaabc40c66adc5875cd0
Emilio Barradas <decalerss@gmail.com>
2023-01-30 00:10:00 -0500

    Sync

--- packages/actions/build/src/build.ts
+++ packages/actions/build/src/build.ts
@@ -1,6 +1,8 @@
 import { CerezaClient, StartBuildCommand } from "client";
 
 export const build = async (url: string, providerId: number, token: string) => {
+	console.log(url, providerId, token);
+
 	const cereza = new CerezaClient({ backend: { url } });
 
 	const build = await cereza.send(
